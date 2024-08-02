eabb1c71503f1985016a6d8dfeda9f7d72cd264c
Daniyal <Daniyal@DESKTOP-UODOQ15>
2024-08-02 17:46:30 +0500

    office work

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace DotNetCorePracticeApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            var myEmployees = new List<Model.Employee>
+            {
+                new Model.Employee { EmpId = 101, Name = "Daniyal" , Position = "Boss", Salary = 100000},
+                new Model.Employee { EmpId = 102, Name = "Shamas" , Position = "Manager", Salary = 10000},
+                new Model.Employee { EmpId = 103, Name = "Farzan" , Position = "Tea Boy", Salary = 1000},
+                new Model.Employee { EmpId = 104, Name = "Nomi" , Position = "Peon", Salary = 100},
+            };
+
+            ViewData["myEmployee"] = myEmployees;
+
+            return View();
+        }
+    }
+}
---
+++ Model/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace DotNetCorePracticeApp.Model
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+
+        public string Name { get; set; }
+        public string Position { get; set; }
+
+        public int Salary { get; set; }
+
+    }
+}
